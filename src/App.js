3cefe70e215edb32e622d93358dfdeecd03fa1d4
Jose Luiz Coe <jose.souza@catho.com>
2019-06-08 00:52:18 -0300

    Exemplo usando redirect

--- src/App.js
+++ src/App.js
@@ -4,16 +4,24 @@ import Home from './components/Home';
 import About from './components/About';
 import Navbar from './components/Navbar';
 import Projects from './components/Projects';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 class App extends Component {
   render() {
+    const loggedIn = true;
     return (
       <div className="App">
         <Navbar />
 
         <Switch>
-          <Route exact path='/' component={Home}/>
+          <Route exact path="/" render={() => (
+            loggedIn ? (
+              <Redirect to="/dashboard" component={Home}/>
+            ) : (
+              <Home/>
+            )
+          )}/>
+          {/* <Route exact path='/' component={Home}/> */}
           <Route path='/about' component={About}/>
           <Route exact path='/projects' component={Projects}/>
         </Switch>
